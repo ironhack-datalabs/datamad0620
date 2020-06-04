1443b2bb31e1f05a053e01a518ae22f5ea6d11bf
jorge doncel <jdoncel27@gmail.com>
2020-06-04 17:44:39 +0200

    hasta el 13

--- module-1/lab-errhand_listcomp/your-code/main.py
+++ module-1/lab-errhand_listcomp/your-code/main.py
@@ -1,3 +1,5 @@
+"""
+
 #Example: 
 
 eggs = (1,3,8,3,2)
@@ -5,6 +7,7 @@ eggs = (1,3,8,3,2)
 my_listComprehension = [1/egg for egg in eggs]
 
 print(my_listComprehension)
+
 
 #Insert here the module/library import statements 
 
@@ -14,38 +17,43 @@ print(my_listComprehension)
 #1. Calculate the square number of the first 20 numbers. Use square as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-
-
+square = [n**2 for n in range(20)]
+print(square)
 
 #2. Calculate the first 50 power of two. Use power_of_two as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-
+power_of_two = [2**n  for n in range(50)]
+print(power_of_two)
 
 
 #3. Calculate the square root of the first 100 numbers. Use sqrt as the name of the list.
 # You will probably need to install math library with pip and import it in this file.  
 # Remember to use list comprehensions and to print your results
 
+import math
 
-
+sqrt = [math.sqrt(n) for n in range(100)]
+print(sqrt)
 
 #4. Create this list [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0]. Use my_list as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-
+my_list= [i for i in range(-10,1)]
+print(my_list)
 
 
 #5. Find the odd numbers from 1-100. Use odds as the name of the list. 
 # Remember to use list comprehensions and to print your results
 
-
-
+odds = [i for i in range(1,101) if i%2 ==0]
+print(odds)
 
 #6. Find all of the numbers from 1-1000 that are divisible by 7. Use divisible_by_seven as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-
+divisible_by_seven = [i for i in range(1,1000) if i%7==0]
+print(divisible_by_seven)
 
 
 #7. Remove all of the vowels in a string. Hint: make a list of the non-vowels. Use non_vowels as the name of the list.
@@ -53,6 +61,9 @@ print(my_listComprehension)
 # You can use the following test string but feel free to modify at your convenience
 
 teststring = 'Find all of the words in a string that are monosyllabic'
+vocales = ["a","e", "i", "o", "u"]
+non_vowels = [i for i in teststring if i not in vocales]
+print(novocales)
 
 
 
@@ -61,6 +72,9 @@ teststring = 'Find all of the words in a string that are monosyllabic'
 # Use capital_letters as the name of the list.  
 # Remember to use list comprehensions and to print your results
 
+sentence = "The Quick Brown Fox Jumped Over The Lazy Dog"
+capital_letters = [i for i in sentence if i.isupper()]
+print(capital_letters)
 
 
 
@@ -68,6 +82,10 @@ teststring = 'Find all of the words in a string that are monosyllabic'
 # Use consonants as the name of the list.
 # Remember to use list comprehensions and to print your results.
 
+sentence = 'The quick brown fox jumped over the lazy dog'
+vocales = ["a","e", "i", "o", "u"]
+consonantes = [i for i in sentence if i not in vocales]
+print(consonantes)
 
 
 
@@ -76,22 +94,32 @@ teststring = 'Find all of the words in a string that are monosyllabic'
 # You will probably need to import os library and some of its modules. You will need to make some online research.
 # Remember to use list comprehensions and to print your results.
 
+import os
+os.chdir("/home/jorge/Escritorio/Ironhack/Ejercicios/datamad0620")
+list= os.listdir()
+print(list)
+
 
 
 #11. Create 4 lists of 10 random numbers between 0 and 100 each. Use random_lists as the name of the list. 
 #You will probably need to import random module
 # Remember to use list comprehensions and to print your results
 
+import random
+random_lists = [random.sample(range(1, 101), 10) for i in range(4)]
+print(random_lists)
 
 
-
+"""
 #12. Flatten the following list of lists. Use flatten_list as the name of the output.
 # Remember to use list comprehensions and to print your results
 
 list_of_lists = [[1,2,3],[4,5,6],[7,8,9]]
+flatten_list = [n for i in list_of_lists for n in i]
 
+print(flatten_list)
 
-
+"""
 #13. Convert the numbers of the following nested list to floats. Use floats as the name of the list. 
 # Remember to use list comprehensions and to print your results.
 
@@ -189,3 +217,4 @@ Total_Marks = int(input("Enter Total Marks Scored: "))
 Num_of_Sections = int(input("Enter Num of Sections: "))
 
 
+"""
