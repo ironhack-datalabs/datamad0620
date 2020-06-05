dad15f0630f6686a6c0de5445003ac06a2e820b0
Nerea Amezcua Valmala <nereaamezcuav@gmail.com>
2020-06-05 20:06:49 +0200

    SAXON

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -33,17 +33,53 @@ class Viking(Soldier):
         return "Odin Owns You All!"
 
 
-    
 
 # Saxon
-
-
-
-class Saxon:
-    pass
-
+class Saxon(Soldier):
+    
+    def __init__(self, health, strength):
+        super().__init__(health, strength)
+        
+    def receiveDamage(self, damage):
+        self.health = self.health - damage
+        if self.health > 0:
+            return f"A Saxon has received {damage} points of damage"
+        else:
+            return f"A Saxon has died in combat"
+"""
 # War
-
-
+import random 
 class War:
-    pass
+    def __init__(self):
+
+        self.vikingArmy = []
+        self.saxonArmy =[]
+
+    def addViking(self, Viking):
+        
+        self.vikingArmy.append(Viking())
+    
+    def addSaxon(self, Saxon):
+        self.saxonArmy.append(Saxon())
+
+    def VikingAttack(self):
+        Saxon_random= random.random(self.saxonArmy)
+        Saxon.receiveDamage() = Viking.strength()
+        if Saxon.health < 0:
+           self.saxonArmy.remove(self.Saxon)
+
+    def saxonAttack(self):
+        Viking_random= random.random(self.vikingArmy)
+        Viking.receiveDamage(self) = Saxon.strength()
+        if Viking.health < 0:
+           self.vikingArmy.remove(self.Viking)
+
+
+    def showStatus(self):
+        if self.saxonArmy == []:
+            return "Vikings have won the war of the century!"
+        elif self.vikingArmy == []:
+            return "Saxons have fought for their lives and survive another day..."
+        else:
+            return "Vikings and Saxons are still in the thick of battle."
+"""
