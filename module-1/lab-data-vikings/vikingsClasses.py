644a23e1ed110f1156aad577f834589412ce7564
Adriana Coca <coca.adriana@outlook.com>
2020-06-05 17:14:33 +0200

    test soldier passed

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -2,8 +2,17 @@
 # Soldier
 
 
-class Soldier:
-    pass
+class Soldier():
+    def __init__ (self, health, strength):
+        self.health=health
+        self.strength=strength
+     
+    def attack(self):
+         return self.strength
+
+    def receiveDamage(self,damage):
+        self.health=self.health-damage
+
 
 # Viking
 
