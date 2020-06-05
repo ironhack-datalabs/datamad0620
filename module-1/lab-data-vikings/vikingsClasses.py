cdbb65b50c105f5d7d9b72c1643e0b93386aa491
Antonio Sánchez <antoniosm1988@gmail.com>
2020-06-05 19:49:27 +0200

    Primer commit Vikingos terminados

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -3,13 +3,42 @@
 
 
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+
+        self.health=health
+        self.strength=strength
+    def attack(self):
+
+        return self.strength
+    
+    def receiveDamage(self,damage):
+
+        self.health -= damage
+
+        pass
+    
 
 # Viking
 
 
-class Viking:
-    pass
+class Viking(Soldier):
+    def  names(self,name):
+        self.name=name
+        self.health=health
+        self.strength=strength
+    def attack(Soldier):
+        return self.strength
+    def receiveDamage(self,damage):
+        if (self.health - damage)<=0:
+            return "{name} has died in act of combat"
+        if (self.health - damage)<0:
+            return "{name} has received {damage} points of damage"
+    def battleCry(self):
+        self.battleCry = "Odin Owns You All!"
+        pass
+
+
+        
 
 # Saxon
 
