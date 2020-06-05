48f8f42f9d5d4b9b82f8ec29ed751a845ad1925d
Carlos Estevez <estevez.carlos10@gmail.com>
2020-06-05 20:39:40 +0200

    viernessss

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -17,37 +17,46 @@ class Soldier:
 
 
 class Viking(Soldier):
-    def __init__(self,name):
+    def __init__(self,name,health,strength):
         super().__init__(health,strength)
         self.name=name
     def receiveDamage(self,damage):
         self.health -=  damage
         if self.health > 0:
-            return f"{name} has received {damage} points of damage"
+            return f"{self.name} has received {damage} points of damage"
         else:
-            return f"{name} has died in act of combat"
+            return f"{self.name} has died in act of combat"
     def battleCry(self):
         return "Odin Owns You All!"
-
 
     pass
 
 # Saxon
 
 class Saxon(Soldier):
-    def __init__(self):
+    def __init__(self,health,strength):
         super().__init__(health,strength)
     def receiveDamage(self,damage):
         self.health -=  damage
         if self.health > 0:
-            return "A Saxon has received {damage} points of damage"
+            return f"A Saxon has received {damage} points of damage"
         else:
-            return "A Saxon has died in act of combat"
+            return "A Saxon has died in combat"
         
     pass
-
+"""
 # War
 
 
 class War:
-    pass
+    vikingArmy=[]
+    saxonArmy=[]
+    def addViking(self,viking="Viking"):
+        self.vikingArmy.append(viking)*10
+    def addSaxon(self,saxon="Saxon"):
+        self.saxonArmy.append(saxon)*10
+    def vikingAttack(self):
+
+
+#### `vikingAttack()` method
+"""
