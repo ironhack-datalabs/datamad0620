7ca52ab39c8151825538c413848fed680e3850f5
Carlos Estevez <estevez.carlos10@gmail.com>
2020-06-05 18:09:50 +0200

    commit

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -1,22 +1,45 @@
 
-# Soldier
-
 
 class Soldier:
     def __init__(self, health, strength):
-            self.health=health
-            self.strength=strength
-    def attack ():
-        strength
-    def receiveDamage():
-        strength
+        self.health=health 
+        self.strength=strength 
+    def attack (self):
+        return self.strength 
+    def receiveDamage(self,damage):
+        self.health= -damage
 
     pass
+
+
+### Viking
+
+A `Viking` is a `Soldier` with an additional property, their `name`. 
+They also have a different `receiveDamage()` method and new method, `battleCry()`.
+
+Modify the `Viking` constructor function, have it inherit from `Soldier`, reimplement the `receiveDamage()` method for `Viking`, 
+and add a new `battleCry()` method.
+
+#### inheritance
+
+- `Viking` should inherit from `Soldier`
+
+#### constructor function
+
+- should receive **3 arguments** (name, health & strength)
+- should receive the **`name` property** as its **1st argument**
+- should receive the **`health` property** as its **2nd argument**
+- should receive the **`strength` property** as its **3rd argument**
 
 # Viking
 
 
-class Viking:
+class Viking(Soldier):
+    def __init__(self, name):
+        super().__init__(health,strength)
+        self.name=name
+
+
     pass
 
 # Saxon
