7000b49bfd21df9fd63b34354e1ec016d93483cb
jorge doncel <jdoncel27@gmail.com>
2020-06-05 18:03:26 +0200

    segundo resulto (falta herencia)

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -16,9 +16,26 @@ class Soldier:
 
 # Viking
 
-
 class Viking:
-    pass
+    def __init__(self, name, health, strength):
+        self.name = name
+        self.health = health
+        self.strength = strength
+    
+    def attack(self):
+        return self.strength
+        
+
+    def receiveDamage(self, thedamage):
+        self.health =  self.health - thedamage
+        if self.health > thedamage:
+            return f"{self.name} has received {thedamage} points of damage"
+        if self.health < thedamage:
+            return f"{self.name} has died in act of combat"
+
+    def battleCry(self):
+        return "Odin Owns You All!"
+
 
 # Saxon
 
