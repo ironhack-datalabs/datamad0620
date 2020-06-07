6894330f01c5b5d0a5d87012e8e9e2b89f892dfd
carlossanz <carsdgp@gmail.com>
2020-06-07 18:28:39 +0100

    no se exactamente como pero funciona

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -1,5 +1,3 @@
-
-
 # Soldier
 
 
@@ -20,16 +18,19 @@ class Soldier:
 class Viking(Soldier):
 
     def __init__(self,name, health, strength):
+        super().__init__(health, strength)
         self.name = name
 
     def receiveDamage(self, damage):
-        self.health -= damage
+        super().receiveDamage(damage)
+           
         if self.health > 0:
             return f"{self.name} has received {damage} points of damage"
-        elif self.health <= 0:
+        else:
             return f"{self.name} has died in act of combat"
+
     def battleCry(self):
-        return "nose que d odin"
+        return "Odin Owns You All!"
 
 
    
@@ -37,103 +38,10 @@ class Viking(Soldier):
 class Saxon(Soldier):
 
     def receiveDamage(self, damage):
-        self.health -= damage
+        super().receiveDamage(damage)
         if self.health > 0:
-            return "A Saxon has received DAMAGE points of damage"
-        elif self.health <= 0:
+            return f"A Saxon has received {damage} points of damage"
+        else:
             return  "A Saxon has died in combat"
         
     
-
-
-
-
-"""
-
-
-
-
-
-# War
-class War:
-    pass
-
-
-
-
-
-
-
-caso practico hecho con Jorge. Clases/objetos/inheritance..)
-es mas o menos igual que el ejercicio
-
-class Soldier:
-    def __init__(self, health, strength):
-        self.health = health
-        self.strength = strength
-
-    def attack(self, anotherone):
-        anotherone.health -=self.strength
-
-    def receiveDamage(self, damage): 
-        self.health -= damage
-
-
-class SoldadoAmericano (Soldier):
-    def __init__(self, health, strength):
-        self.health = health
-        self.strength = strength
-
-
-    def curarse(self):
-        self.health += 50
-
-class SoldierChileno(Soldier):
-    def __init__(self, health, strength):
-        self.health = health
-        self.strength = strength
-
-
-
-  
-
-    def comincarradio(self, mensaje):
-        print ("Enviando", mensaje)
-# Viking
-
-
-viking = SoldierChileno(500, 750)
-
-viking.comincarradio("Nos estan masacrando")
-    
-jaimegonzalez = SoldadoAmericano(800, 900)       
-    
-text = "caraculo"
-
-print(jaimegonzalez.health)
-
-jaimegonzalez.receiveDamage(76)
-
-print(jaimegonzalez.health)
-
-jaimegonzalez.curarse()
-
-print(jaimegonzalez.health)
-
-print (viking.health)
-jaimegonzalez.attack(viking)
-print(viking.health)
-
-# Saxon
-
-
-class Saxon:
-    pass
-
-# War
-
-
-class War:
-    pass
-
-"""
