892882db6127b05aebedf90c860938bbb90ab11f
Carlos Estevez <estevez.carlos10@gmail.com>
2020-06-07 22:30:31 +0200

    final commit

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -52,16 +52,29 @@ class War:
     def __init__(self):
         self.vikingArmy=[]
         self.saxonArmy=[]
-    def addViking(self,viking="Viking"):
-        self.vikingArmy.append(self.viking*10)
-    def addSaxon(self,saxon="Saxon"):
-        self.saxonArmy.append(self.saxon*10)
+    def addViking(self,Viking):
+        self.vikingArmy.append(Viking*10)
+    def addSaxon(self,Saxon):
+        self.saxonArmy.append(Saxon*10)
     def vikingAttack(self):
-
-
+        random_Viking=random.choice(self.vikingArmy)
+        random_Viking(self.strength)=Saxon.receiveDamage()
+        for i in self.vikingArmy:
+            if i(self.health) <=0:
+                self.vikingArmy.remove(i)
     def saxonAttack(self):
-    
+        random_Saxon=random.random(self.saxonArmy)
+        random_Saxon(self.strength)=Viking.receiveDamage()
+        for i in self.saxonArmy:
+            if i(self.health) <=0:
+                self.saxonArmy.remove(i)
     def showStatus(self):
+        if len(self.saxonArmy)==0:
+            return "Vikings have won the war of the century!"
+        elif len(self.vikingArmy)==0:
+            return "Saxons have fought for their lives and survive another day..."
+        else:
+            return "Vikings and Saxons are still in the thick of battle."
 
 
 #### `vikingAttack()` method
