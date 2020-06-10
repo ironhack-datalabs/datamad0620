344f058b058146083bb78f3b2044732110ea1bdc
WHYTEWYLL <juanmiblanco1@gmail.com>
2020-06-10 19:05:49 +0200

    correccion, luego jupyter

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -16,28 +16,29 @@ SELECT  aut.au_id AS AUTHORID,aut.au_lname AS "LAST NAME",aut.au_fname AS "FIRST
  
  
  /* Challegen 3 */
- 
-SELECT AUTHORID, LAST_NAME, FIRST_NAME , COUNT(AUTHORID) AS CUENTITA
- FROM(SELECT  aut.au_id AS AUTHORID,aut.au_lname AS "LAST_NAME",aut.au_fname AS "FIRST_NAME",pu.pub_name AS PUBLISHER ,COUNT(ti.title) AS TITLECOUNT
- FROM authors AS aut
- LEFT JOIN titleauthor AS ta ON aut.au_id=ta.au_id
- LEFT JOIN titles AS ti ON ta.title_id=ti.title_id
- LEFT JOIN publishers AS pu ON ti.pub_id=pu.pub_id
- GROUP BY AUTHORID,PUBLISHER) AS Q 
- GROUP BY AUTHORID 
- ORDER BY CUENTITA DESC LIMIT 3;
- 
-  /* Challegen 4 */
- 
- SELECT AUTHORID, LAST_NAME, FIRST_NAME , COUNT(AUTHORID) AS CUENTITA
- FROM(SELECT  aut.au_id AS AUTHORID,aut.au_lname AS "LAST_NAME",aut.au_fname AS "FIRST_NAME",pu.pub_name AS PUBLISHER ,COUNT(ti.title) AS TITLECOUNT
+   
+  SELECT AUTHORID , LAST_NAME, FIRST_NAME, SUM(N_VENTAS) S_NVENTAS FROM (
+ SELECT  aut.au_id AS AUTHORID,aut.au_lname AS "LAST_NAME",aut.au_fname AS "FIRST_NAME", 
+ ti.advance AS "ADELANTO" , ti.royalty AS PORCENTAJEROYAL,TI.ytd_sales AS N_VENTAS ,ta.au_ord AS N_DEAUTORES
  FROM authors AS aut
    LEFT  JOIN titleauthor AS ta ON aut.au_id=ta.au_id
   LEFT  JOIN titles AS ti ON ta.title_id=ti.title_id
-  LEFT JOIN publishers AS pu ON ti.pub_id=pu.pub_id
- GROUP BY AUTHORID,PUBLISHER) AS Q 
- GROUP BY AUTHORID 
- ORDER BY CUENTITA DESC;
+  LEFT JOIN publishers AS pu ON ti.pub_id=pu.pub_id) AS R
+  GROUP BY AUTHORID
+  ORDER BY S_NVENTAS DESC  LIMIT 3;
+ 
+ 
+  /* Challegen 4 */
+ 
+ SELECT AUTHORID , LAST_NAME, FIRST_NAME, SUM(N_VENTAS) S_NVENTAS FROM (
+ SELECT  aut.au_id AS AUTHORID,aut.au_lname AS "LAST_NAME",aut.au_fname AS "FIRST_NAME", 
+ ti.advance AS "ADELANTO" , ti.royalty AS PORCENTAJEROYAL,TI.ytd_sales AS N_VENTAS ,ta.au_ord AS N_DEAUTORES
+ FROM authors AS aut
+   LEFT  JOIN titleauthor AS ta ON aut.au_id=ta.au_id
+  LEFT  JOIN titles AS ti ON ta.title_id=ti.title_id
+  LEFT JOIN publishers AS pu ON ti.pub_id=pu.pub_id) AS R
+  GROUP BY AUTHORID
+  ORDER BY S_NVENTAS DESC  ;
  
  
  /*BONUS Challegen  */
