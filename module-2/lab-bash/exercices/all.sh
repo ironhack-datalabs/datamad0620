4f343e59c272242c2bb9b34fd28e9d8c035cb1bb
Irene <irenelopezlujan@gmail.com>
2020-06-17 09:24:06 +0200

    3 puntos mas

--- module-2/lab-bash/exercices/all.sh
+++ module-2/lab-bash/exercices/all.sh
@@ -1,3 +1,5 @@
+
+"""
 echo "Hello Word"
 mkdir ../new_dir
 rm -r ../new_dir
@@ -17,18 +19,33 @@ wc -l ../lorem/sed.txt
 find ../../ -name "lorem*"     #repetir que mi archivo de comandos esta en la carpeta exercices no en la principal
 grep -r -i "et" ../lorem/at.txt
 
-"""
+
 explicacion web:
-Para que se entienda fácil, explicamos que significa 
-cada uno de los elementos del comando. «-r» indica que 
-se debe realizar de forma recursiva en todos 
+«-r» indica que se debe realizar de forma recursiva en todos 
 los subdirectorios contenidos en la ubicación 
 proporcionada. «-i» permite omitir las mayúscula 
 y minúsculas, haciendo que la búsqueda sea mucho más
 sencilla. «palabra o palabras» es el texto que
  queremos probar. «/home/redeszone» es la ruta sobre 
  la que se realizará la búsqueda.
- """
+ 
 
  grep -c -r -i "et" ../lorem/at.txt #igual pero -c cuenta
  grep -c -r -i "et" ../lorem-copy/*
+grep -c -r -i "et" lorem-copy/*
+
+variable_name=Irene
+echo $variable_name
+mkdir ../variable_name
+rm -r ../variable_name
+
+carpeta_lorem=../lorem
+"""
+
+#!/bin/bash
+file=../lorem/*
+for x in $file;
+do
+	echo $x;
+	
+done
