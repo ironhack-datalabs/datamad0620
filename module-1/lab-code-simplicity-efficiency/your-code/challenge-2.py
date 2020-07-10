ac2fe0d04e10b0b8f111620c271c72c36876ccd9
Antonio Ortiz <antonio.ortizms@me.com>
2020-07-10 09:59:50 +0200

    challange 1 y 2

--- module-1/lab-code-simplicity-efficiency/your-code/challenge-2.py
+++ module-1/lab-code-simplicity-efficiency/your-code/challenge-2.py
@@ -6,33 +6,54 @@ the strings being generated.
 The code is functional but has a lot of room for improvement. Use what you have learned
 about simple and efficient code, refactor the code.
 """
+import string
+import random
+import sys
+def RandomStringGenerator(l, a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']):
+    #p = 0
+    #s = ''
+    #while p<l:
+    #    import random
+    #    s += random.choice(a)
+    #    p += 1
+    #return s
+    return "".join([random.choice(a) for x in range(l)])
 
-def RandomStringGenerator(l=12, a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']):
-    p = 0
-    s = ''
-    while p<l:
-        import random
-        s += random.choice(a)
-        p += 1
-    return s
+"""
+def inputs():
+    global a
+    a = input('Enter minimum string length: ')
+    global b
+    b = input('Enter maximum string length: ')
+    global n
+    n = input('How many random strings to generate? ')
+    if a == '':
+        a=8
+    if b== '':
+        b=12
+    if n == '':
+        n=12
+    return n,a,b
+"""
 
-def BatchStringGenerator(n, a=8, b=12):
+def BatchStringGenerator(n=12,a=5,b=5):
     r = []
     for i in range(n):
         c = None
         if a < b:
-            import random
             c = random.choice(range(a, b))
         elif a == b:
             c = a
         else:
-            import sys
             sys.exit('Incorrect min and max string lengths. Try again.')
         r.append(RandomStringGenerator(c))
     return r
 
-a = input('Enter minimum string length: ')
-b = input('Enter maximum string length: ')
-n = input('How many random strings to generate? ')
+def runcode():
+    print(BatchStringGenerator())
 
-print(BatchStringGenerator(int(n), int(a), int(b)))
+runcode()
+
+#que horror jaja, estaba intentando hacer que si el usuario no escribiese nada nos quedamos con el valor default. 
+#estoy muy picado, pero te dejo que me ilumines :)!
+#la i parece tener un error, pero funciona...
