03a03665ce5f5261b4c947ffa65042aa3bc47993
Laura <laubarmu@hotmail.com>
2020-07-10 09:24:00 +0200

    chll1

--- module-1/lab-code-simplicity-efficiency/your-code/challenge-1.py
+++ module-1/lab-code-simplicity-efficiency/your-code/challenge-1.py
@@ -8,7 +8,7 @@ expects.
 The code is very long and messy. Refactor it according to what you have learned about
 code simplicity and efficiency.
 """
-
+'''
 print('Welcome to this calculator!')
 print('It can add and subtract whole numbers from zero to five')
 a = input('Please choose your first number (zero to five): ')
@@ -166,4 +166,23 @@ if a == 'five' and b == 'minus' and c == 'five':
 if (not a == 'zero' and not a == 'one' and not a == 'two' and not a == 'three' and not a == 'four' and not a == 'five') or (not c == 'zero' and not c == 'one' and not c == 'two' and not c == 'three' and not c == 'four' and not c == 'five') or (not b == 'plus' and not b == 'minus'):
     print("I am not able to answer this question. Check your input.")
 
-print("Thanks for using this calculator, goodbye :)")
+print("Thanks for using this calculator, goodbye :)")'''
+
+print('Welcome to this calculator!')
+print('It can add and subtract whole numbers from zero to five')
+a = input('Please choose your first number (zero to five): ')
+b = input('What do you want to do? plus or minus: ')
+c = input('Please choose your second number (zero to five): ')
+
+numbers=[:6]
+def operations():
+    if a in numbers and c in numbers:
+        if b == "plus":
+            a + c
+        if b == "minus":
+            a - c
+        else:
+            print('This operation can not be done')
+
+    return f'{a}{b}{c}
+    print("Thanks for using this calculator, goodbye :)")    
